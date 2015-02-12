319dbd45997f9bedc89d41775724fcb87558a472
Bryan Cardillo <bryan.cardillo@gmail.com>
2015-02-12 14:17:26 -0500

    add documentation header and logo

--- src/main/resources/templates/components/masthead.cs
+++ src/main/resources/templates/components/masthead.cs
@@ -1,2 +1,11 @@
 <?cs # disable masthead ?>
-<?cs def:custom_masthead() ?><?cs /def ?>
+<?cs def:custom_masthead() ?>
+<div id="header">
+  <div id="headerLeft">
+    <img src="../../assets/images/joinery.png" alt="Joinery" width="48" height="48" />
+  </div>
+  <div id="headerRight">
+    <span id="masthead-title">Joinery &mdash; Data frames for Java</span>
+  </div>
+</div>
+<?cs /def ?>
