11972894a733165d68eb4d0122bb5cdfe895c7de
Bryan Cardillo <bryan.cardillo@gmail.com>
2015-02-20 03:30:27 -0500

    add column alignment and other tostring improvements

--- src/main/java/joinery/impl/Serialization.java
+++ src/main/java/joinery/impl/Serialization.java
@@ -25,11 +25,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import joinery.DataFrame;
 
@@ -40,29 +43,82 @@ import org.supercsv.io.CsvListWriter;
 import org.supercsv.prefs.CsvPreference;
 
 public class Serialization {
+
+    private static final String EMPTY_DF_STRING = "[empty data frame]";
+    private static final String ELLIPSES = "...";
+    private static final String NEWLINE = "\n";
+    private static final String DELIMITER = "\t";
+    private static final Object INDEX_KEY = new Object();
+    private static final int    MAX_COLUMN_WIDTH = 20;
+
     public static String toString(final DataFrame<?> df, final int limit) {
         final int len = df.length();
-        final StringBuilder sb = new StringBuilder();
 
-        for (final Object column : df.columns()) {
-            sb.append("\t");
-            sb.append(String.valueOf(column));
+        if (len == 0) {
+            return EMPTY_DF_STRING;
         }
-        sb.append("\n");
 
+        final StringBuilder sb = new StringBuilder();
+        final Map<Object, Integer> width = new HashMap<>();
+        final List<Class<?>> types = df.types();
+        final List<Object> columns = new ArrayList<>(df.columns());
+
+        // determine index width
+        width.put(INDEX_KEY, 0);
+        for (final Object row : df.index()) {
+            width.put(INDEX_KEY, clamp(
+                    width.get(INDEX_KEY),
+                    MAX_COLUMN_WIDTH,
+                    String.valueOf(row).length()));
+        }
+
+        // determine column widths
+        for (int c = 0; c < columns.size(); c++) {
+            final Object column = columns.get(c);
+            width.put(column, String.valueOf(column).length());
+            for (int r = 0; r < df.length(); r++) {
+                width.put(column, clamp(
+                        width.get(column),
+                        MAX_COLUMN_WIDTH,
+                        fmt(types.get(c), df.get(r, c)).length()));
+            }
+        }
+
+        // output column names
+        sb.append(lpad("", width.get(INDEX_KEY)));
+        for (int c = 0; c < columns.size(); c++) {
+            sb.append(DELIMITER);
+            final Object column = columns.get(c);
+            sb.append(center(column, width.get(column)));
+        }
+        sb.append(NEWLINE);
+
+        // output rows
         final Iterator<Object> names = df.index().iterator();
         for (int r = 0; r < len; r++) {
-            sb.append(String.valueOf(names.hasNext() ? names.next() : r));
-            for (int c = 0; c < df.size(); c++) {
-                sb.append("\t");
-                sb.append(String.valueOf(df.get(r, c)));
-            }
-            sb.append("\n");
+            // output row name
+            int w = width.get(INDEX_KEY);
+            sb.append(truncate(lpad(names.hasNext() ? names.next() : r, w), w));
 
+            // output rows
+            for (int c = 0; c < df.size(); c++) {
+                sb.append(DELIMITER);
+                final Class<?> cls = types.get(c);
+                w = width.get(columns.get(c));
+                if (Number.class.isAssignableFrom(cls)) {
+                    sb.append(lpad(fmt(cls, df.get(r, c)), w));
+                } else {
+                    sb.append(truncate(rpad(fmt(cls, df.get(r, c)), w), w));
+                }
+            }
+            sb.append(NEWLINE);
+
+            // skip rows if necessary to limit output
             if (limit - 3 < r && r < (limit << 1) && r < len - 4) {
-                sb.append("\n... ");
-                sb.append(len - limit);
-                sb.append(" rows skipped ...\n\n");
+                sb.append(NEWLINE).append(ELLIPSES)
+                  .append(" ").append(len - limit)
+                  .append(" rows skipped ").append(ELLIPSES)
+                  .append(NEWLINE).append(NEWLINE);
                 while (r < len - 2) {
                     if (names.hasNext()) {
                         names.next();
@@ -73,6 +129,63 @@ public class Serialization {
         }
 
         return sb.toString();
+    }
+
+    private static final int clamp(final int lower, final int upper, final int value) {
+        return Math.max(lower, Math.min(upper, value));
+    }
+
+    private static final String lpad(final Object o, final int w) {
+        final StringBuilder sb = new StringBuilder();
+        final String value = String.valueOf(o);
+        for (int i = value.length(); i < w; i++) {
+            sb.append(' ');
+        }
+        sb.append(value);
+        return sb.toString();
+    }
+
+    private static final String rpad(final Object o, final int w) {
+        final StringBuilder sb = new StringBuilder();
+        final String value = String.valueOf(o);
+        sb.append(value);
+        for (int i = value.length(); i < w; i++) {
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+
+    private static final String center(final Object o, final int w) {
+        final StringBuilder sb = new StringBuilder();
+        final String value = String.valueOf(o);
+        for (int i = 0; i < (w >> 1); i ++) {
+            sb.append(' ');
+        }
+        sb.append(value);
+        for (int i = sb.length(); i < w; i++) {
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+
+    private static final String truncate(final Object o, final int w) {
+        final String value = String.valueOf(o);
+        return value.length() > w ? value.substring(0, w - 3) + ELLIPSES : value;
+    }
+
+    private static final String fmt(final Class<?> cls, final Object o) {
+        String s;
+        if (o instanceof Number) {
+            if (Short.class.equals(cls) || Integer.class.equals(cls) ||
+                    Long.class.equals(cls) || BigInteger.class.equals(cls)) {
+                s = String.format("%d", Number.class.cast(o).longValue());
+            } else {
+                s = String.format("%.8f", Number.class.cast(o).doubleValue());
+            }
+        } else {
+            s = String.valueOf(o);
+        }
+        return s;
     }
 
     public static DataFrame<Object> readCsv(final String file)
