d59c69b391ee36bfd2fc6c88c26ea3e95c64b1d6
Bryan Cardillo <bryan.cardillo@gmail.com>
2015-02-20 03:46:49 -0500

    add v1.2 docs

---
+++ v1.2/api/reference/lists.js
@@ -0,0 +1,13 @@
+var DATA = [
+      { id:0, label:"joinery", link:"joinery/package-summary.html", type:"package" },
+      { id:1, label:"joinery.DataFrame", link:"joinery/DataFrame.html", type:"class" },
+      { id:2, label:"joinery.DataFrame.Aggregate", link:"joinery/DataFrame.Aggregate.html", type:"class" },
+      { id:3, label:"joinery.DataFrame.Function", link:"joinery/DataFrame.Function.html", type:"class" },
+      { id:4, label:"joinery.DataFrame.JoinType", link:"joinery/DataFrame.JoinType.html", type:"class" },
+      { id:5, label:"joinery.DataFrame.KeyFunction", link:"joinery/DataFrame.KeyFunction.html", type:"class" },
+      { id:6, label:"joinery.DataFrame.PlotType", link:"joinery/DataFrame.PlotType.html", type:"class" },
+      { id:7, label:"joinery.DataFrame.Predicate", link:"joinery/DataFrame.Predicate.html", type:"class" },
+      { id:8, label:"joinery.DataFrame.RowFunction", link:"joinery/DataFrame.RowFunction.html", type:"class" },
+      { id:9, label:"joinery.DataFrame.SortDirection", link:"joinery/DataFrame.SortDirection.html", type:"class" }
+
+    ];
