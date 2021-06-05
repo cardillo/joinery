0caa78e0d2f8472274b845af0ffd11821fe6c12c
Bryan Cardillo <bryan.cardillo@gmail.com>
2021-06-05 14:53:21 -0500

    add v1.10 docs

---
+++ v1.10/api/assets/navtree_data.js
@@ -0,0 +1,9 @@
+var NAVTREE_DATA =
+[ [ "joinery", "joinery/package-summary.html", [ [ "Interfaces", null, [ [ "DataFrame.Aggregate", "joinery/DataFrame.Aggregate.html", null, "" ], [ "DataFrame.Function", "joinery/DataFrame.Function.html", null, "" ], [ "DataFrame.KeyFunction", "joinery/DataFrame.KeyFunction.html", null, "" ], [ "DataFrame.Predicate", "joinery/DataFrame.Predicate.html", null, "" ], [ "DataFrame.RowFunction", "joinery/DataFrame.RowFunction.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "DataFrame", "joinery/DataFrame.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "DataFrame.Axis", "joinery/DataFrame.Axis.html", null, "" ], [ "DataFrame.JoinType", "joinery/DataFrame.JoinType.html", null, "" ], [ "DataFrame.NumberDefault", "joinery/DataFrame.NumberDefault.html", null, "" ], [ "DataFrame.PlotType", "joinery/DataFrame.PlotType.html", null, "" ], [ "DataFrame.SortDirection", "joinery/DataFrame.SortDirection.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
