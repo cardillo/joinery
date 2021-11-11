081ff76e84e37e922cf1456bcdce14d2021f1cde
Dongming Xia <dongmingxia@DongmingdeMacBook-Pro.local>
2021-11-11 11:50:11 -0800

    add more random testing

--- src/test/java/examples/Random.java
+++ src/test/java/examples/Random.java
@@ -1,5 +1,6 @@
 package examples;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,21 +9,29 @@ import joinery.DataFrame;
 public class Random {
     public static void main(final String[] args){
         DataFrame<Object> df1 = new DataFrame<>("0", "1", "2");
-        df1.append("0", Arrays.asList(10, 20, 30));
-        df1.append("1", Arrays.asList(40, 50, 60));
-        System.out.println(df1);
+        df1.append(Arrays.asList(1, 2, 3));
+        df1.append(Arrays.asList(4, 5, 6));
+        //System.out.println(df1);
 
         DataFrame<Object> df2 = new DataFrame<>("3", "4", "5");
-        df2.append("0", Arrays.asList(40, 50, 60));
-        df2.append("1",Arrays.asList(70, 80, 90));
-        System.out.println(df2);
+        df2.append(Arrays.asList(10, 20, 30));
+        df2.append(Arrays.asList(40, 50, 60));
+        //System.out.println(df2);
 
-        // If we want to concat two dataframes, we can use the append function and add rows one by one
-
-        for(List<Object> row: df2){
-            df1.append(row);
-        }
+        df1 = df1.concatenate(df2, 0);
 
         System.out.println(df1);
+
+        DataFrame<Object> df3 = new DataFrame<>("a", "b", "c");
+        df3.append(Arrays.asList(1, 2, 3));
+        df3.append(Arrays.asList(4, 5, 6));
+
+        DataFrame<Object> df4 = new DataFrame<>("x", "y", "z");
+        df4.append(Arrays.asList(10, 20, 30));
+        df4.append(Arrays.asList(40, 50, 60));
+
+        df3 = df3.concatenate(df4, 1);
+        System.out.println(df3);
+
     }
 }
